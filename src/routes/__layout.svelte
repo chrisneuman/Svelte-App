797e30bc1e08c84432f1c10b2af908202b4877d4
johnychan <caneuman@gmail.com>
2022-08-12 16:22:55 -0500

    Update __layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,7 +10,7 @@
 </main>
 
 <footer>
-	<p>visit <a rel="external" href="http://madatyourmom.com">MadAtYourMom.com</a></p>
+	<p>visit <a rel="external" href="http://www.madatyourmom.com">MadAtYourMom.com</a></p>
 	
 
 </footer>
